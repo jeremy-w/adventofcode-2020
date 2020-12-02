a59b2098b9f2cda06a660f9723088fb84bcefc50
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2020-12-01 21:19:03 -0500

    Trying to sort out stdin reading

--- src/day1.m
+++ src/day1.m
@@ -17,14 +17,41 @@ Test case:
 514579
 */
 :- module day1.
+
 :- interface.
 :- import_module io.
 :- pred main(io::di, io::uo) is det.
+
 :- implementation.
+:- import_module string, int, list, stream, char.
+
+:- pred cons_as_int(string::in, list(int)::in, list(int)::out) is det.
+cons_as_int(Line, Accu0, Accu) :-
+    Num = string.det_to_int(Line),
+    Accu = [Num | Accu0].
+
+:- pred char_list_cons(char::in, list(char)::in, list(char)::out) is det.
+char_list_cons(X, Xs, [ X | Xs ]).
 
 main(!IO) :-
-    % read stdin, one number per line, into an array
+    % read stdin, one number per line, into a list.
+    Stream = io.stdin_stream,
+    stream.name(Stream, Name, !IO),
+    Numbers = [1, 2, 3, 4],
+    stream.input_stream_fold(Stream, char_list_cons, [], PartialResult, !IO),
+    % (
+    %     PartialResult = ok(Result),
+    %     io.write(Result, !IO),
+    %     io.nl(!IO)
+    % ;
+        io.write(PartialResult, !IO),
+        io.nl(!IO),
+    % ),
+
     % smart bruteforce search:
     % - sort the array large => small
     % - for each from start to end, start summing from end to start, and bail when exceeding 2020
-    io.write_string("Hi!\n", !IO).
+    io.write_string(Name, !IO),
+    io.nl(!IO),
+    io.write(Numbers, !IO),
+    io.nl(!IO).
