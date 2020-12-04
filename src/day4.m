9da75176b74795f1dafb3131b146f6743e0bc0bb
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2020-12-04 13:26:52 -0500

    Read in the example for day 4

--- src/day4.m
+++ src/day4.m
@@ -5,7 +5,30 @@
 :- pred main(io::di, io::uo) is det.
 
 :- implementation.
-:- import_module string, char, list.
+:- import_module string, char, list, require, util.
 
 main(!IO) :-
+    test_example(!IO),
     io.print_line("it builds", !IO).
+
+%----%
+%----%
+
+:- func requiredFields = list(string).
+requiredFields = [
+    "byr",
+    "iyr",
+    "eyr",
+    "hgt",
+    "hcl",
+    "ecl",
+    "pid"
+    % "cid",  % conveniently not required
+].
+
+%---%
+
+:- pred test_example(io::di, io::uo) is det.
+test_example(!IO) :-
+    util.read_file_as_string("../input/day4.example.txt", Input, !IO),
+    io.write_line(Input, !IO).
