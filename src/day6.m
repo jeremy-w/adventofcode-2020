15bf63c831f480bdfe6046f62e16658291159ef1
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2020-12-06 09:13:30 -0500

    Solved day 6 part 2
    Part 1 answer: they sum to: 6686
    Part 2 answer: they sum to: 3476

--- src/day6.m
+++ src/day6.m
@@ -5,13 +5,23 @@
 :- pred main(io::di, io::uo) is det.
 
 :- implementation.
-:- import_module util, string, char, list, int.
+:- import_module util, string, char, list, int, set, std_util.
 
 :- func group_to_answers(string) = string.
 group_to_answers(Group) = Answers :-
     WithoutNewlines = replace_all(Group, "\n", ""),
     Uniqued = remove_adjacent_dups(sort(to_char_list(WithoutNewlines))),
     Answers = from_char_list(Uniqued).
+
+:- func group_to_all_answers(string) = string.
+group_to_all_answers(Group) = Answers :-
+    PerPerson: list(list(char)) = map(to_char_list, split_at_string("\n", strip(Group))),
+    Sets = map(list_to_set, PerPerson),
+    trace [io(!IO), runtime(env("TRACE"))] (io.write_line(Sets, !IO)),
+    % XXX: Initially folded with set.init, but that gave the empty set each time!
+    AllAnsweredYesSet = foldl(set.intersect, Sets, det_head(Sets)),
+    Answers = from_char_list(set.to_sorted_list(AllAnsweredYesSet)).
+
 
 :- func plus(int, int) = int.
 plus(X, Y) = X + Y.
@@ -22,4 +32,8 @@ main(!IO) :-
     YesAnswers = map(group_to_answers, Groups),
     Lengths = map(string.length, YesAnswers),
     Sum = list.foldl(day6.plus, Lengths, 0),
-    io.format("Part 1 answer: they sum to: %d", [i(Sum)], !IO).
+    io.format("Part 1 answer: they sum to: %d\n", [i(Sum)], !IO),
+
+    EveryAnswers = map(group_to_all_answers, Groups),
+    Part2: int = list.foldl(day6.plus, list.map(string.length, EveryAnswers), 0),
+    io.format("Part 2 answer: they sum to: %d\n", [i(Part2)], !IO).
