32015b2b22e1fd1f0c56df21ce432bfd191a8d1c
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2020-12-07 20:58:59 -0500

    Day 7 part 1 done!
    Part1 Test: Expected 4, found 4
    Part1 Answer: 224

--- src/day7.m
+++ src/day7.m
@@ -5,7 +5,7 @@
 :- pred main(io::di, io::uo) is det.
 
 :- implementation.
-:- import_module string, list, char, int.
+:- import_module string, list, char, int, util.
 
 % Lets us say "BagX contained_by BagY-WithMultiplicity for multiple BagY-WithMultiplicity pairs.
 % Or ColorY contains ColorX-WithMultiplicity.
@@ -17,7 +17,10 @@ my_bag = "shiny gold".
 
 main(!IO) :-
     TestCount = part1(my_bag, example_rules),
-    io.format("Part1 Test: Expected 4, found %d", [i(TestCount)], !IO).
+    io.format("Part1 Test: Expected 4, found %d\n", [i(TestCount)], !IO),
+    util.read_file_as_string("../input/day7.txt", Input, !IO),
+    Part1Count = part1(my_bag, Input),
+    io.format("Part1 Answer: %d\n", [i(Part1Count)], !IO).
 
 :- func example_rules = string.
 example_rules = "light red bags contain 1 bright white bag, 2 muted yellow bags.
@@ -35,7 +38,7 @@ dotted black bags contain no other bags.
 :- func compile_rules(string) = {contains_map, contained_by_map}.
 compile_rules(RuleText) = {Contains, ContainedBy} :-
     Rules = condense(filter_map(parse_rule, split_at_string("\n", RuleText))),
-    trace [io(!IO)] (io.write_line(Rules, !IO)),
+    trace [io(!IO), runtime(env("TRACE_RULES"))] (io.write_line(Rules, !IO)),
     foldl2((pred({Container, N, Contained}::in, !.ContainsMap::in, !:ContainsMap::out, !.ContainedMap::in, !:ContainedMap::out) is det :-
         add(Container, {N, Contained}, !ContainsMap),
         add(Contained, {Container, N}, !ContainedMap)), Rules, init, Contains, init, ContainedBy).
@@ -63,13 +66,13 @@ part1(BagToHold, RuleText) = ColorCount :-
 % Returns the colors that contain the color.
 :- func reaches(contained_by_map, string) = list(string).
 reaches(M, Color) = Colors :-
-    trace [io(!IO)] (io.format("what reaches Color=%s?\n", [s(Color)], !IO)),
+    trace [io(!IO), runtime(env("TRACE_REACHES"))] (io.format("what reaches Color=%s?\n", [s(Color)], !IO)),
     (if
         search(M, Color, Match)
     then
         Seed = map((func({C, N}) = C), Match),
         Reaches = Seed ++ condense(map(reaches(M), Seed)),
         Colors = remove_adjacent_dups(sort(Reaches)),
-        trace [io(!IO)] (io.format("reaching Color=%s: ", [s(Color)], !IO), io.write_line(Colors, !IO))
+        trace [io(!IO), runtime(env("TRACE_REACHES"))] (io.format("reaching Color=%s: ", [s(Color)], !IO), io.write_line(Colors, !IO))
     else
         Colors = []).
