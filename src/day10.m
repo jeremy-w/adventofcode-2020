9c1e2be29518c674db6326de983b8405d6086dc4
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2020-12-10 07:54:31 -0500

    Solved day 10 part 1
    ===[ day10 ]===
    P1 test: expected 35, got 35
    P1 test b: expected 22 1-jolt, 10 3-jolt, for 220, got 220
    P1: got 1917

--- src/day10.m
+++ src/day10.m
@@ -11,13 +11,13 @@ main(!IO) :-
     io.format("===[ %s ]===\n", [s($module)], !IO),
     % Test: Part 1
     Expected1 = 7*5,
-    Actual1 = part1(example1),
+    Actual1 = part1(ints(example1)),
     io.format("P1 test: expected %d, got %d\n", [i(Expected1), i(Actual1)], !IO),
 
-    io.format("P1 test b: expected 22 1-jolt, 10 3-jolt, for %d, got %d\n", [i(22*10), i(part1(example1b))], !IO),
+    io.format("P1 test b: expected 22 1-jolt, 10 3-jolt, for %d, got %d\n", [i(22*10), i(part1(ints(example1b)))], !IO),
 
     util.read_file_as_string("../input/day10.txt", Input, !IO),
-    Input = _.
+    io.format("P1: got %d\n", [i(part1(ints(Input)))], !IO).
 
 :- func example1 = string.
 example1 = "16
@@ -67,5 +67,31 @@ example1b = "28
 3
 ".
 
-:- func part1(string) = int.
-part1(_) = 42.
+:- func part1(list(int)) = int.
+part1(Joltages) = ProductOf1And3Deltas :-
+    InOrder = sort(Joltages),
+    Pairs = chunk(zip(InOrder, det_tail(InOrder)), 2),
+    Deltas = filter_map((func([H, T]) = T - H is semidet), Pairs),
+    foldl2((pred(Delta::in, Ones0::in, Ones::out, Threes0::in, Threes::out) is det :-
+        ( if Delta = 1
+        then
+            Ones = Ones0 + 1,
+            Threes = Threes0
+        else if Delta = 3
+        then
+            Threes = Threes0 + 1,
+            Ones = Ones0
+        else
+            Threes = Threes0,
+            Ones0 = Ones
+        )), Deltas, 0, OnesCount, 0, ThreesCount),
+    trace [io(!IO), runtime(env("TRACE"))] (
+        io.write_line({"input", Joltages}, !IO),
+        io.write_line({"in order", InOrder}, !IO),
+        io.write_line({"pairs", Pairs}, !IO),
+        io.write_line({"deltas", Deltas}, !IO),
+        io.write_line({"stats", 1, OnesCount, 3, ThreesCount}, !IO)
+    ),
+    OnesCountIncludingOutlet = OnesCount + 1,
+    ThreesCountIncludingDevice = ThreesCount + 1,
+    ProductOf1And3Deltas = OnesCountIncludingOutlet * ThreesCountIncludingDevice.
