4f4ea47061c0224d040ddd929b1bd5980ccc8b4c
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2020-12-10 07:18:18 -0500

    Solved part 9 day 2
    Whoops, forgot to add this!
    
    ===[ day9 ]===
    P1 test: expected 127, got 127
    P1: 133015568
    P2 test: expected 62, got 62
    P2: 16107959

--- src/day9.m
+++ src/day9.m
@@ -11,12 +11,19 @@ main(!IO) :-
     io.format("===[ %s ]===\n", [s($module)], !IO),
     % Test: Part 1
     Expected1 = expected1,
-    Actual1 = part1(example1, 5),
+    Actual1 = part1(ints(example1), 5),
     io.format("P1 test: expected %d, got %d\n", [i(Expected1), i(Actual1)], !IO),
 
     util.read_file_as_string("../input/day9.txt", Input, !IO),
-    P1 = part1(Input, 25),
-    io.format("P1: %d\n", [i(P1)], !IO).
+    Ns = ints(Input),
+    P1 = part1(Ns, 25),
+    io.format("P1: %d\n", [i(P1)], !IO),
+
+    E2 = 62,
+    A2 = part2(ints(example1), 5),
+    io.format("P2 test: expected %d, got %d\n", [i(E2), i(A2)], !IO),
+    P2 = part2(Ns, 25),
+    io.format("P2: %d\n", [i(P2)], !IO).
 
 :- func example1 = string.
 example1 = "35
@@ -45,9 +52,8 @@ example1 = "35
 expected1 = 127.
 
 % find the first number in the list (after the preamble) which is not the sum of two of the 25 numbers before it. What is the first number that does not have this property?
-:- func part1(string, int) = int.
-part1(Input, PrefixLength) = Result :-
-    Ns = ints(Input),
+:- func part1(list(int), int) = int.
+part1(Ns, PrefixLength) = Result :-
     Tails = tails(Ns),
     filter_map(take(PrefixLength+1), [Ns | Tails], PrefixAndToCheck),
     % trace [io(!IO)] (io.write_line(PrefixAndToCheck, !IO)),
@@ -69,6 +75,33 @@ sum_of(N, Ns) :-
     delete_first(Ns, X, Rest),
     member(Y, Rest),
     N = X + Y.
+
+%---%
+
+:- func part2(list(int), int) = int.
+part2(Ns, PrefixLength) = EncryptionWeakness :-
+    Key = part1(Ns, PrefixLength),
+    ( if ContiguousRange = range_summing_to(Key, Ns) then
+        % To find the encryption weakness, add together the smallest and largest number in this contiguous range; in this example, these are 15 and 47, producing 62.
+        SortedRange = sort(ContiguousRange),
+        Min = det_head(SortedRange),
+        Max = det_last(SortedRange),
+        EncryptionWeakness = Min + Max
+    else
+        EncryptionWeakness = -1
+    ).
+
+% find a contiguous set of at least two numbers in your list which sum to the invalid number from step 1
+% TODO: find this contiguous range
+:- func range_summing_to(int, list(int)) = list(int) is semidet.
+range_summing_to(Key, Ns) = R :-
+    WindowLengths = 2 .. length(Ns),
+    map((pred(WindowLength::in, WindowsOfLength::out) is det :- filter_map(take(WindowLength), [Ns | tails(Ns)], WindowsOfLength)), WindowLengths, Windowses),
+    condense(Windowses, Windows),
+    find_first_match((pred(Window::in) is semidet :- foldl(plus, Window, 0) = Key), Windows, R).
+
+%---%
+% LIST UTILITIES
 
 :- func ints(string) = list(int).
 ints(Input) = Ns :-
