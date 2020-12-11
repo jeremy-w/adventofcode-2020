0d12114676c9f387f031585ebb2af2f0a3a0812d
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2020-12-11 10:48:29 -0500

    Got day 11 p 1
    ===[ day11 ]===
    P1 test: expected 37, got 37
    P1: got 2310
    === * ===

--- src/day11.m
+++ src/day11.m
@@ -19,14 +19,14 @@ empty = 'L'.
 main(!IO) :-
     io.format("===[ %s ]===\n", [s($module)], !IO),
     % Test: Part 1
+    SeatMapE1 = parse_seat_map(example1),
+    % io.write_line(step(SeatMapE1), !IO),
     Expected1 = 37,
-    Actual1 = part1(parse_seat_map(example1)),
+    Actual1 = part1(SeatMapE1),
     io.format("P1 test: expected %d, got %d\n", [i(Expected1), i(Actual1)], !IO),
 
-    % io.format("P1 test b: expected 22 1-jolt, 10 3-jolt, for %d, got %d\n", [i(22*10), i(part1(ints(example1b)))], !IO),
-
-    % util.read_file_as_string("../input/day11.txt", Input, !IO),
-    % io.format("P1: got %d\n", [i(part1(ints(Input)))], !IO),
+    util.read_file_as_string("../input/day11.txt", Input, !IO),
+    io.format("P1: got %d\n", [i(part1(parse_seat_map(Input)))], !IO),
 
     % io.format("P2 test: expected %d, got %d\n", [i(8), i(part2(ints(example1)))], !IO),
     % io.format("P2 test 2: expected %d, got %d\n", [i(19208), i(part2(ints(example1b)))], !IO),
@@ -110,31 +110,45 @@ L.#.L..#..
 ].
 
 :- func part1(seat_map) = int.
-part1(seat_map(_NRows, _NCols, Rows)) = OccupiedSeatCount :-
-    % repeat(step, seat_map, StableSeatMap),
-    RowLists = map(to_char_list, Rows),
+part1(S) = OccupiedSeatCount :-
+    repeat_till_stable(S, StableSeatMap),
+    OccupiedSeatCount = occupied_seat_count(StableSeatMap).
+
+:- pred repeat_till_stable(seat_map::in, seat_map::out) is det.
+repeat_till_stable(S0, S) :-
+    S1 = step(S0),
+    (if S0 = S1
+    then S = S1
+    else repeat_till_stable(S1, S)).
+
+:- func occupied_seat_count(seat_map) = int.
+occupied_seat_count(S) = OccupiedSeatCount :-
+    RowLists = map(to_char_list, S^rows),
     RowCounts = map((func(Seats) = Count :-
         OccupiedOnly = filter(unify(occupied), Seats),
         Count = length(OccupiedOnly)), RowLists),
     OccupiedSeatCount = foldl(plus, RowCounts, 0).
 
 :- func step(seat_map) = seat_map.
-step(S) = seat_map(S^nrows, S^ncols, S1) :-
-    S1 = S^rows.
-    % for R0 in 0 .. (S^nrows - 1)
-    %     for C0 in 0 .. (S^ncols - 1)
-    %         SNext = step_at(S, R0, C0)
+step(S) = S1 :-
+    AllRows = 0 .. (S^nrows - 1),
+    S1 = foldl(step_row(S), AllRows, S).
 
-:- func step_at(seat_map, int, int) = seat_map.
-step_at(S, R0, C0) = SNext :-
+:- func step_row(seat_map, int, seat_map) = seat_map.
+step_row(S, R0, SAcc) = SNext :-
+    AllCols = 0 .. (S^ncols - 1),
+    SNext = foldl(step_at(S, R0), AllCols, SAcc).
+
+:- func step_at(seat_map, int, int, seat_map) = seat_map.
+step_at(S, R0, C0, SAcc) = SNext :-
     ( if at(S, R0, C0, Was)
     then
         Around = around(S, R0, C0),
         OccupiedAround = length(filter(unify(occupied), Around)),
         WillBe = next(Was, OccupiedAround),
-        update(S, R0, C0, WillBe, SNext)
+        update(SAcc, R0, C0, WillBe, SNext)
     else
-        SNext = S
+        SNext = SAcc
     ).
 
 :- func next(char, int) = char.
@@ -170,9 +184,10 @@ at(seat_map(_NRows, _NCols, Rows), R0, C0, C) :-
 
 :- pred update(seat_map::in, int::in, int::in, char::in, seat_map::out) is det.
 update(S0, R0, C0, C, S) :-
+    % trace [io(!IO)] (io.write_line({"update", S0, R0, C0, C}, !IO)),
     list.det_index0(S0^rows, R0, Row),
     NewRow = det_set_char(C, C0, Row),
-    NewRows = det_replace_nth(S0^rows, R0, NewRow),
+    NewRows = det_replace_nth(S0^rows, R0+1, NewRow),
     S = S0^rows := NewRows.
 
 :- func parse_seat_map(string) = seat_map.
