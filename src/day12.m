5687c48ca0fa7988ecd3430175a06bb88bc26cc5
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2020-12-11 19:40:44 -0500

    Stub out day12

---
+++ src/day12.m
@@ -0,0 +1,25 @@
+:- module day12.
+:- interface.
+:- import_module io.
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+:- import_module char, int, list, string, util.
+:- import_module solutions.
+
+main(!IO) :-
+    io.format("===[ %s ]===\n", [s($module)], !IO),
+
+    Example = "",
+    E1 = 35,
+    A1 = part1(Example),
+    io.format("P1 test: expected %d, got %d\n", [i(E1), i(A1)], !IO),
+
+    % util.read_file_as_string("../input/day12.txt", Input, !IO),
+    % P1 = part1(Input),
+    % io.format("P1: got %d\n", [i(P1)], !IO),
+
+    io.print_line("=== * ===", !IO).
+
+:- func part1(string) = int.
+part1(_) = 10.
