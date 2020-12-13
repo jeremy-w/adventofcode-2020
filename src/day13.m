13e9360e423c2123d199e6d39466d6c819e51e1f
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2020-12-13 05:56:45 -0500

    Did day 13 part 1
    Used Pythonista on my phone. Input set is so small you could solve it
    by hand!

--- src/day13.m
+++ src/day13.m
@@ -23,3 +23,17 @@ main(!IO) :-
 
 :- func part1(string) = int.
 part1(_) = 10.
+
+/*
+I did part 1 on my phone with Pythonista because the input was teensy.
+
+t=1008141
+s='17,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,523,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,13,19,x,x,x,23,x,x,x,x,x,x,x,787,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,29'
+y = s.split(',')
+ys = [int(n) for n in y if n != 'x']
+zs = [(n * math.ceil(t / n) - t, n) for n in ys]
+sorted(zs)
+6*787
+# 4722
+
+*/
