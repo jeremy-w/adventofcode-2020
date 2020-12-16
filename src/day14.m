7f48479b3b65b20a9a0c993de7aeab85d533107d
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2020-12-15 20:50:37 -0500

    Begin filling in day14

--- src/day14.m
+++ src/day14.m
@@ -4,16 +4,24 @@
 :- pred main(io::di, io::uo) is det.
 
 :- implementation.
-:- import_module char, int, list, string, util.
-:- import_module solutions.
+:- import_module char.
+:- import_module list.
+:- import_module map.
+:- import_module string.
+:- import_module uint.
+:- import_module util.
 
 main(!IO) :-
     io.format("===[ %s ]===\n", [s($module)], !IO),
 
-    Example = "",
-    E1 = 35,
+    Example = "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0
+",
+    E1 = 165u,
     A1 = part1(Example),
-    io.format("P1 test: expected %d, got %d\n", [i(E1), i(A1)], !IO),
+    io.format("P1 test: expected %u, got %u\n", [u(E1), u(A1)], !IO),
 
     % util.read_file_as_string("../input/day14.txt", Input, !IO),
     % P1 = part1(Input),
@@ -21,5 +29,20 @@ main(!IO) :-
 
     io.print_line("=== * ===", !IO).
 
-:- func part1(string) = int.
-part1(_) = 10.
+:- func part1(string) = uint.
+part1(Input) = SumOfMemValues :-
+    SumOfMemValues = 0u.
+
+
+:- type addr == uint.
+
+    % Values and memory addresses are both 36-bit unsigned integers.
+:- type machine
+    ---> machine(
+                % addr => value
+            mem :: map(addr, uint),
+                % for anding
+            mask_set :: uint,
+                % for and-notting
+            mask_clear :: uint
+        ).
